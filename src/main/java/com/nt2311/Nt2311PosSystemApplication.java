cc467fa258cf06aabcd47087ac5f7e279bec892e
ntcachyos <nguyentoan231196@gmail.com>
2025-10-20 21:30:54 +0700

    commit the POS repo into github

---
+++ src/main/java/com/nt2311/Nt2311PosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nt2311;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nt2311PosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nt2311PosSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nt2311/Nt2311PosSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt2311;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nt2311PosSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
