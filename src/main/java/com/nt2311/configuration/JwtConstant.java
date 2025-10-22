cab264f95829873788f4f0279bb73d983f4d4b0a
ntcachyos <nguyentoan231196@gmail.com>
2025-10-22 21:36:47 +0700

    add class for constant of jwt

---
+++ src/main/java/com/nt2311/configuration/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.nt2311.configuration;
+
+public class JwtConstant {
+  public static final String JWT_SECRET = "z124dsadsoGSSD@zx123dkowmnv";
+  public static final String JWT_HEADER = "Authorization";
+}
