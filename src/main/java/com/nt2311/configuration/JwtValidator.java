68e5edadfe15b185481dd2e3d0213345e5a898d2
ntcachyos <nguyentoan231196@gmail.com>
2025-12-03 20:12:28 +0700

    JwtValidator

--- src/main/java/com/nt2311/configuration/JwtValidator.java
+++ src/main/java/com/nt2311/configuration/JwtValidator.java
@@ -8,7 +8,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 import javax.crypto.SecretKey;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 public class JwtValidator extends OncePerRequestFilter {
@@ -27,8 +33,12 @@ public class JwtValidator extends OncePerRequestFilter {
 
         String email = String.valueOf(claims.get("email"));
         String authorities = String.valueOf(claims.get("authorities"));
+        List<GrantedAuthority> auths =
+            AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+
+        Authentication auth = new UsernamePasswordAuthenticationToken(email, null, auths);
+        SecurityContextHolder.getContext().setAuthentication(auth);
       } catch (Exception e) {
-        // TODO: handle exception
       }
     }
   }
